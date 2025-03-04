b43542d5d3637ec379a1d7eb5ef64d1d65851485
Boar-ingCode <dzikuudev@gmail.com>
2025-03-04 19:39:56 +0100

    Bulidng first layer of architecture

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import json
+import random
+import nltk
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, TensorDataset
+
+class ChatBotManual(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(ChatBotManual, self).__init__()
+
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, output_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
